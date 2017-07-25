17936ab6ac2e2594a0d3ece2e775940c164dcae7
Denylson Melo <denylsonmelo@gmail.com>
2017-07-25 18:10:35 -0300

    adicionando terceira questao da prova

---
+++ provas/src/main/java/br/edu/ifpi/capar/poo/provas/bimestre1/Questao03.java
@@ -0,0 +1,14 @@
+package br.edu.ifpi.capar.poo.provas.bimestre1;
+
+/**Questão retirada do site AprovaConcursos
+ * do concurso IESES - 2015 - TRE-MA - Técnico Judiciário - Programação de Sistemas
+ * (com modificações)
+ * @see <a href="https://www.aprovaconcursos.com.br/questoes-de-concurso/questao/383430">Link da questão no AprovaConcursos</a>
+ */
+public class Questao03 {
+    public static void main(String[] args) {
+        int i = 9;
+        int x = 10;
+        System.out.println("Soma " + i + x);
+    }
+}

fc1099709d3719fb1949168061f23b7a7d0a13a6
Denylson Melo <denylsonmelo@gmail.com>
2017-09-15 12:23:42 -0300

    tarefa: como fazer o total?

--- oracle-academy/src/main/java/br/edu/ifpi/capar/poo/oracle/academy/CalculadoraFestaAlex.java
+++ oracle-academy/src/main/java/br/edu/ifpi/capar/poo/oracle/academy/CalculadoraFestaAlex.java
@@ -6,40 +6,22 @@ package br.edu.ifpi.capar.poo.oracle.academy;
  */
 public class CalculadoraFestaAlex {
 
-    private static double VALOR_IMPOSTO = 0.05;
-    protected static final double VALOR_GORJETA = 0.15;
-    public double valor = 0;
-
-    void encontrarTotalAPagar() {
-        System.out.println("person: $" + this.valor * (1 + VALOR_IMPOSTO + VALOR_GORJETA));
-    }
+    private static final double VALOR_IMPOSTO = 0.05;
+    private static final double VALOR_GORJETA = 0.15;
     
     void encontrarTotalAPagar(double valor) {
-        
         System.out.println("person: $" + valor * (1 + VALOR_IMPOSTO + VALOR_GORJETA));
-    
     }
 
     void encontrarTotalAPagar(int valor) {
         System.out.println("person: $" + valor * (1 + VALOR_IMPOSTO + VALOR_GORJETA));
-        //return 0;
     }
 
     void encontrarTotalAPagar(int valor, double valorImposto) {
-        System.out.println("person: $" + valor * (1 + valorImposto));
-        //return 0;
+        System.out.println("person: $" + valor * (1 + valorImposto + VALOR_GORJETA));
     }
-    
-    void metodo(String... variavel){
-        
-        
-        for (String minhaVariavelzinha : variavel) {
-            System.out.println(minhaVariavelzinha);
-        }
-        
-        /*  diferenca dos for
-        for (int i = 0; i < variavel.length; i++) {
-            System.out.println(variavel[i]);
-        }*/
+
+    void encontrarTotalAPagar(double valor, double valorImposto) {
+        System.out.println("person: $" + valor * (1 + valorImposto + VALOR_GORJETA));
     }
 }
--- oracle-academy/src/main/java/br/edu/ifpi/capar/poo/oracle/academy/CalculadoraFestaAlexTest.java
+++ oracle-academy/src/main/java/br/edu/ifpi/capar/poo/oracle/academy/CalculadoraFestaAlexTest.java
@@ -9,40 +9,19 @@ import java.util.Scanner;
 public class CalculadoraFestaAlexTest {
     public static void main(String[] args) {
         
-        Scanner scanner = new Scanner(System.in);
-        
         CalculadoraFestaAlex galas = new CalculadoraFestaAlex();
         CalculadoraFestaAlex tawan = new CalculadoraFestaAlex();
         CalculadoraFestaAlex eric = new CalculadoraFestaAlex();
-        
-        /*
-        System.out.println("galas " + calculadora1.valor);
-        System.out.println("tawan " + calculadora2.valor);
-        
-        calculadora1.valor = 100;   // 120
-        calculadora1.encontrarTotalAPagar();
        
-        System.out.println("galas " + calculadora1.valor);
-        System.out.println("tawan " + calculadora2.valor);
         
-        calculadora1.valor = 200;    //240
-        calculadora1.encontrarTotalAPagar();
+        int totalGalas = galas.encontrarTotalAPagar(1000);
         
-        calculadora2.valor = 300;    //360
-        calculadora2.encontrarTotalAPagar();
+        tawan.encontrarTotalAPagar(2000, 0.05);
         
-        //   ??????????
+        eric.encontrarTotalAPagar(1000.2, 0.4);
         
-        calculadora1.VALOR_IMPOSTO = 2; // nao pode pois é publico
-        
-        calculadora1.encontrarTotalAPagar();
-*/
-        
-        galas.encontrarTotalAPagar(100, 0.3);
-        
-        galas.metodo("1", "2", "3", "alguma coisa");
-        
-        
+        //imprimir o total
+        System.out.println();
         
         
     }
